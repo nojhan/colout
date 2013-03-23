45872df75cfcb8a1d8bfe88392280dc6abd99f41
nojhan <nojhan@nojhan.net>
2013-03-23 18:12:31 +0100

    Use pygments in the g++ theme

--- colout_g++.py
+++ colout_g++.py
@@ -1,4 +1,3 @@
-#encoding: utf-8
 import colout
 def theme( item ):
     item = colout.colorup( item, "error", "red", "bold" )
@@ -7,7 +6,7 @@ def theme( item ):
     item = colout.colorup( item, "note", "blue", "bold" )
     item = colout.colorup( item, ":([0-9]+):[0-9]*", "yellow", "normal" )
     item = colout.colorup( item, "^((/\w+)+)\.(h|cpp)", "white", "normal" )
-    item = colout.colorup( item, "'(.*)'", "blue", "normal" )
+    item = colout.colorup( item, "'(.*)'", "Cpp", "monokai" )
 
     return item
 

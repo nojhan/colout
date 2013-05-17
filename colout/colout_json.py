1fed20cba7b7fee45f2f72e1486c3bcd04c74017
Johann Dreo <johann.dreo@thalesgroup.com>
2013-05-17 11:13:53 +0200

    Expect formatted json as input, blue numbers
    This patch avoid coloring what's inside item's strings, but expect a formatted input.

--- colout/colout_json.py
+++ colout/colout_json.py
@@ -1,8 +1,12 @@
 
 def theme():
+    # This theme expect a formatted JSON input, with items spread across lines.
+    # See tools like "python -m json.tool" or "json_xs"
     return [
-        [ '[][{}]' ],
-        [ '[:,]', "yellow" ],
+        [ '[\[\]{}],*\s*\n' ],
+        [ '" (:) ', "yellow" ],
+        [ '[\]}"](,)', "yellow" ],
+        [ "\"(-*[0-9]+\.*[0-9]*e*-*[0-9]*)\"", "blue" ],
         [ '"(.*)"', "green" ],
         [ """["']""", "cyan" ]
     ]

410aaf16237694b9fc645435620230822a80e3a5
Johann Dreo <johann.dreo@thalesgroup.com>
2013-10-10 15:56:24 +0200

    add the "orange" and "purple" color synonyms

--- colout/colout.py
+++ colout/colout.py
@@ -138,8 +138,8 @@ styles = {
 
 # Available color names in 8-colors mode
 colors = {
-    "black": 0, "red": 1, "green": 2, "yellow": 3, "blue": 4,
-    "magenta": 5, "cyan": 6, "white": 7, "none": -1
+    "black": 0, "red": 1, "green": 2, "yellow": 3, "orange":3, "blue": 4,
+    "magenta": 5, "purple": 5, "cyan": 6, "white": 7, "none": -1
 }
 
 themes = {}

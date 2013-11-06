483bb886d7a84eb165a460c1260e0d181ddb9f77
nojhan <nojhan@nojhan.net>
2013-11-06 11:23:32 +0100

    bugfix path match for python theme

--- colout/colout_python.py
+++ colout/colout_python.py
@@ -5,7 +5,7 @@ def theme():
             ["^Traceback .*$", "blue" ],
             # File, line, in
             [
-                "^\s{2}(File \")(/.*?/)*([^/:]+)(\", line) ([0-9]+)(, in) (.*)$",
+                "^\s{2}(File \")(/*.*?/)*([^/:]+)(\", line) ([0-9]+)(, in) (.*)$",
                 "blue,  none,  white,blue,  yellow,blue",
                 "normal,normal,bold, normal,normal,bold"
             ],

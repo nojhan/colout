3def9779a4718a00706ed41553dc50313284a6d7
nojhan <nojhan@nojhan.net>
2014-02-21 11:01:40 +0100

    Add a theme for java

---
+++ colout/colout_java.py
@@ -0,0 +1,15 @@
+#encoding: utf-8
+
+def theme():
+    style="monokai"
+    return [
+            [ "^(.*\.java):([0-9]+):\s*(warning:.*)$", "white,yellow,magenta", "normal,normal,bold" ],
+            [ "^(.*\.java):([0-9]+):(.*)$", "white,yellow,red", "normal,normal,bold" ],
+            [ "^(symbol|location)\s*:\s*(.*)$", "blue,Java", "bold,"+style ],
+            [ "^(found)\s*:\s*(.*)", "red,Java", "bold,"+style ],
+            [ "^(required)\s*:\s*(.*)", "green,Java", "bold,"+style ],
+            [ "^\s*\^$", "cyan", "bold" ],
+            [ "^\s+.*$", "Java", style ],
+            [ "[0-9]+ error[s]*", "red", "bold" ],
+            [ "[0-9]+ warning[s]*", "magenta", "bold" ],
+        ]

7d589a68babd6428868d586e1cd51a9371581f9f
nojhan <nojhan@nojhan.net>
2014-03-14 21:28:03 +0100

    Adds a LaTeX theme

---
+++ colout/colout_latex.py
@@ -0,0 +1,26 @@
+
+def theme():
+    return [
+            # LaTeX
+            ["This is .*TeX.*$", "white", "bold"],
+            ["(LaTeX Warning): (.*) `(.*)' on page [0-9] (.*) on input line [0-9]+.$",
+                "magenta,magenta,white,magenta", "normal,bold,normal" ],
+            ["(LaTeX Warning): (.*)", "magenta", "normal,bold" ],
+            # ["on (page [0-9]+)", "yellow", "normal" ],
+            ["on input (line [0-9]+)", "yellow", "normal" ],
+            ["^! .*$", "red", "bold"],
+            ["(.*erfull) ([^\s]+).* in [^\s]+ at (lines [0-9]+--[0-9]+)",
+                "magenta,magenta,yellow", "normal"],
+            ["\\[^\s]+\s", "white", "bold"],
+            ["^l\.([0-9]+) ", "yellow"],
+            ["(Output written on) (.*) \(([0-9]+ pages), [0-9]+ bytes\).",
+                "blue,white,blue", "normal,bold,normal"],
+            ["WARNING.*", "magenta", "normal"],
+            ["warning.*", "magenta", "normal"],
+
+            # BiBTeX
+            ["^(I couldn't) (.*)", "red", "normal,bold"],
+            ["(I found) no (.*)", "red"],
+            ["^---(line [0-9]+) of file (.*)", "yellow,white", "normal"],
+        ]
+

7d848d09e8b5152c804e325b70099274dc88787f
Oliver Bristow <obristow@mintel.com>
2018-08-04 18:26:17 +0100

    Add package classifiers

--- setup.py
+++ setup.py
@@ -18,9 +18,22 @@ requires = ['argparse; python_version < "2.7"', 'pygments', 'babel']
 
 setup_requires = ['setuptools_scm']
 
+classifiers = """
+Environment :: Console
+License :: OSI Approved :: GNU General Public License v3 (GPLv3)
+Programming Language :: Python :: 2
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.4
+Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3.7
+""".strip().split('\n')
+
 setup(
     name='colout',
     use_scm_version=True,
+    classifiers=classifiers,
     description='Color Up Arbitrary Command Output.',
     long_description=open('README.md').read(),
     author='nojhan',

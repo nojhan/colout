e49b42db45a810c41524f9508d3a7bba0525832c
Oliver Bristow <obristow@mintel.com>
2018-08-04 18:43:50 +0100

    Add 3.3 classifier

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ License :: OSI Approved :: GNU General Public License v3 (GPLv3)
 Programming Language :: Python :: 2
 Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3
+Programming Language :: Python :: 3.3
 Programming Language :: Python :: 3.4
 Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.6
